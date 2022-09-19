1946ee34436e28514c33a8310c8969f95d1b5f0c
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-09-19 16:21:46 +0300

    add animation

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,7 @@ import * as flsFunctions from "./modules/functions.js";
 import "./calculate-select.js";
 import "./burger-menu.js";
 import "./slider.js";
+import "./validator.js";
+import "./animation.js";
 
 flsFunctions.isWebp();
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,11 @@
 import $ from "jquery";
 import "slick-carousel";
 
+if (window.innerWidth < 768) {
+  $(".problems-list").slick({});
+}
+
 $(".team__body").slick({
   fade: true,
+  cssEase: "linear",
 });
-
-if (window.innerWidth < 768) {
-  $(".problems-list").slick();
-}
